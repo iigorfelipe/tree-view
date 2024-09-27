7ec274a14fb785528719844a7045c8b1bf7ead66
Igor <iigorfelipe@gmail.com>
2024-09-27 12:22:46 -0300

    feat: implements Loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export const Loading = () => {
+  const [dots, setDots] = useState('.');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + '.' : '.'));
+    }, 985);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="text-center">      
+      <img className="size-11 animate-bounce" src="/cap-icon.svg" />
+      <p className="text-2xl font-bold text-primary">{dots}</p>
+    </div>
+  );
+};
