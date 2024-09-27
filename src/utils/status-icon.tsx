b7ab22c02b22b8f044d79fad8836ff5311d424aa
Igor <iigorfelipe@gmail.com>
2024-09-27 13:22:57 -0300

    feat: implements StatusIcon

---
+++ src/utils/status-icon.tsx
@@ -0,0 +1,22 @@
+import { TreeNode } from '../types/api';
+
+type StatusIconProps = {
+  componentSelected: TreeNode | undefined;
+};
+
+export const StatusIcon = ({ componentSelected }: StatusIconProps) => {
+  if (!componentSelected) return undefined;
+
+  return (
+    <img
+      src={
+        componentSelected.status === 'alert'
+          ? '/alert.svg'
+          : componentSelected.sensorType === 'energy'
+          ? '/energy.svg'
+          : '/operating.svg'
+      }
+      alt={componentSelected.type}
+    />
+  );
+};
