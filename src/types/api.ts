9cdbe124096d0dfe972225a33461fc114b384674
Igor <iigorfelipe@gmail.com>
2024-09-27 11:23:11 -0300

    chore: add api types

---
+++ src/types/api.ts
@@ -0,0 +1,34 @@
+export type Company = {
+  id: string;
+  name: string;
+};
+
+export type Asset = {
+  id: string;
+  name: string;
+  parentId?: string | null;
+  sensorId?: string;
+  sensorType?: string;
+  status?: string;
+  locationId?: string | null;
+  gatewayId?: string | null;
+};
+
+export type Location = {
+  id: string;
+  name: string;
+  parentId: string | null;
+};
+
+export type TreeNode = {
+  id: string;
+  name: string;
+  parentId: string | null;
+  type: string;
+  children: TreeNode[];
+  sensorId?: string;
+  sensorType?: string;
+  status?: string;
+  locationId?: string | null;
+  gatewayId?: string | null;
+};
