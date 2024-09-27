ba2b09fc3906c321f30e4e84edaa0c7a2cbbab10
Igor <iigorfelipe@gmail.com>
2024-09-27 12:51:54 -0300

    feat: implements selectOptions to select input

---
+++ src/utils/selectOptions.ts
@@ -0,0 +1,9 @@
+export type SelectOptionKeys = "all" | "energy" | "alert" | "operating" | "vibration"
+
+export const selectOptions = {
+  all: 'Todos',
+  energy: 'Sensor de Energia',
+  alert: 'Crítico',
+  operating: 'Operando',
+  vibration: 'Sensor de Vibração',
+};
