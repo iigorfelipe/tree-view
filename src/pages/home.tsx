3831eadccd1510f5b742e379948991dba0857f30
Igor <iigorfelipe@gmail.com>
2024-09-28 18:41:03 -0300

    refactor: chages the home page image

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,23 @@
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '../contexts/theme';
-import headerMobile from '/header-mobile.png';
-import headerWeb from '/header.jpg';
 
 export const Home = () => {
   const { isSmDown } = useTheme();
   const navigate = useNavigate();
 
   return (
-    <>
-      <div className="relative">
-        <img src={isSmDown ? headerMobile : headerWeb} alt="header image" className="w-full h-auto" />
+    <div className="bg-home h-[90vh] w-full flex items-center justify-center px-8">
+      <div className="relative ">
+        <img src="/home-msg.png" alt="home message" className="w-full h-auto" />
         {isSmDown && (
           <button
             onClick={() => navigate('/companies')}
-            className="cursor-pointer bg-transparent border-none p-0 absolute bottom-5 left-2/4 transform -translate-x-2/4"
+            className="cursor-pointer bg-transparent border-none p-0 absolute -bottom-2/3 left-2/4 transform -translate-x-2/4"
           >
             <img src="/next.svg" alt="arrow next" />
           </button>
         )}
       </div>
-    </>
+    </div>
   );
 };
