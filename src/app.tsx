5a8a98ef6317e3e48b1ec36cdf58896e4c035bf1
Igor <iigorfelipe@gmail.com>
2024-09-29 10:38:30 -0300

    fix home path

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: '/tree-view',
+        path: '/',
         element: <Home />,
       },
       {
